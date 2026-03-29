bbfdd187d9024cc9e2db12c4d077c7972f003a12
Pratibha666 <pratibha.yadav1128@gmail.com>
2026-03-29 13:48:28 +0530

    resolving email service error

--- server/config/sendEmail.js
+++ server/config/sendEmail.js
@@ -5,7 +5,7 @@ const BREVO_API_URL = "https://api.brevo.com/v3/smtp/email";
 const sendEmail = async ({ to, subject, html }) => {
   try {
     const response = await axios.post(
-      BREVO_API_KEY,
+      BREVO_API_URL,
       {
         sender: {
           name: process.env.BREVO_SENDER_NAME || "Safar",
